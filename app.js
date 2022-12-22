9b8d00a439739d59cc6bbd6a40e6171bd15387f8
mahesh <mahesh.202104008@tulas.edu.in>
2022-12-22 21:53:33 +0530

    updated

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const path = require('path');
 
 app.use(bodyParser.json());
 app.use(cookieParser('ssh!!!! some secret string'));
-app.use(csrf('this_should_be_32_character_long',['POST','PUT','DELETE']));
+app.use(csrf('this_should_be_32_character_long', ['POST', 'PUT', 'DELETE']));
 
 
 // seting the ejs is the engine
@@ -24,9 +24,10 @@ app.get('/', async (request, response)=>{
   const overdue = await Todo.overdue();
   const dueToday = await Todo.dueToday();
   const dueLater = await Todo.dueLater();
+  const completedItems = await Todo.completedItems();
   if (request.accepts('html')) {
     response.render('index', {
-      allTodos, overdue, dueToday, dueLater,
+      allTodos, overdue, dueToday, dueLater, completedItems,
       csrfToken: request.csrfToken(),
     });
   } else {
@@ -70,11 +71,27 @@ app.put('/todos/:id/markAsCompleted', async (request, response)=>{
   }
 });
 
-
+// app.delete('/todos/:id', (request, response)=>{
+//   console.log('Delete a todo by id', request.params.id);
+// });
+app.put('/todos/:id', async (request, response) => {
+  const todo = await Todo.findByPk(request.params.id);
+  try {
+    const upTodo = await todo.setCompletionStatus(request.body.completed);
+    return response.json(upTodo);
+  } catch (error) {
+    return response.status(422).json(error);
+  }
+});
 
 app.delete('/todos/:id', async function(request, response) {
   console.log('We have to delete a Todo with ID: ', request.params.id);
-  
+  // FILL IN YOUR CODE HERE
+
+  // First, we have to query our database to delete a Todo by ID.
+  // eslint-disable-next-line max-len
+  // Then, we have to respond back with true/false based on whether the Todo was deleted or not.
+  // response.send(true)
   const deleteFlag = await Todo.destroy({where: {id: request.params.id}});
   response.send(deleteFlag ? true : false);
 });
