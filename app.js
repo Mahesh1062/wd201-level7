09437ed1c3c04b7998651a82024da7558f75c74e
Mahesh1062 <113339637+Mahesh1062@users.noreply.github.com>
2022-12-22 15:22:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const path = require('path');
 
 app.use(bodyParser.json());
 app.use(cookieParser('ssh!!!! some secret string'));
-app.use(csrf({cookie: true}));
+app.use(csrf('this_should_be_32_character_long',['POST','PUT','DELETE']));
 
 
 // seting the ejs is the engine
